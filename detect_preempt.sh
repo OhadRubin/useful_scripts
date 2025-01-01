5e879e471c859c0a67cf28c3295d77948e3a359d
Your Name <you@example.com>
2025-01-01 06:44:24 +0000

    ..

--- detect_preempt.sh
+++ detect_preempt.sh
@@ -1,1 +1,1 @@
-curl "http://metadata.google.internal/computeMetadata/v1/instance/preempted" -H "Metadata-Flavor: Google"
+curl "http://metadata.google.internal/computeMetadata/v1/instance/preempted?wait_for_change=true" -H "Metadata-Flavor: Google"
