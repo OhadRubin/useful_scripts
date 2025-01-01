249221336ca73a3a8f162ecdab26cf2a73203ecb
Your Name <you@example.com>
2025-01-01 06:47:01 +0000

    ..

--- detect_preempt.sh
+++ detect_preempt.sh
@@ -1,1 +1,13 @@
-curl "http://metadata.google.internal/computeMetadata/v1/instance/preempted?wait_for_change=true" -H "Metadata-Flavor: Google"
+detect_preempt() {
+  curl "http://metadata.google.internal/computeMetadata/v1/instance/preempted?wait_for_change=true" -H "Metadata-Flavor: Google"
+  echo "Preempted" > /tmp/preempted.txt
+}
+
+detect_preempt &
+
+while [ ! -f /tmp/preempted.txt ]; do
+  sleep 1
+  echo "Waiting for preemption..."
+done
+echo "Preempted"
+exit 0
