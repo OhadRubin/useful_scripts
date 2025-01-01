6bb50454e04ebe46b6c12b4d122bbfdb2e89d737
Your Name <you@example.com>
2025-01-01 08:26:21 +0000

    ..

--- detect_preempt.sh
+++ detect_preempt.sh
@@ -1,13 +1,18 @@
 detect_preempt() {
   curl "http://metadata.google.internal/computeMetadata/v1/instance/preempted?wait_for_change=true" -H "Metadata-Flavor: Google"
-  echo "Preempted" > /tmp/preempted.txt
+  /home/ohadr/.local/bin/alerts msg --message "Preempted"
+#   echo "Preempted" > /tmp/preempted.txt
 }
 
 detect_preempt &
 
-while [ ! -f /tmp/preempted.txt ]; do
-  sleep 1
-  echo "Waiting for preemption..."
-done
-echo "Preempted"
-exit 0
+# while [ ! -f /tmp/preempted.txt ]; do
+#   sleep 1
+#   echo "Waiting for preemption..."
+# done
+# echo "Preempted"
+# exit 0
+
+# gcloud compute instances simulate-maintenance-event v4-4-node-64 --zone=us-central2-b --with-extended-notifications=True
+
+# gcloud alpha compute tpus tpu-vm simulate-maintenance-event v4-4-node-64 --zone us-central2-b --workers=all --with-extended-notifications=True
